b599dff1afe33fa8e695b6568cb9abd3d957a657
CWS <nyxbappa@gmail.com>
2026-06-30 14:20:46 +0700

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "╔═══════════════════════════════════════════╗"
+echo "║   CWS UNINSTALLER                        ║"
+echo "╚═══════════════════════════════════════════╝"
+echo ""
+make uninstall
+rm -rf ~/CWS
+echo "[+] CWS REMOVED"
+echo "[+] GOODBYE, WEAKLING"
